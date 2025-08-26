08446c0b6b26543ee310b804053384a920edbcda
Davi Isaac Galhardo <davi_galhardo@educadventista.org>
2025-08-26 08:27:04 -0300

    Atualizando estrutura do projeto

--- dw2-DaviIsaacGalhardo---biblioteca/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-export function setRoutes(app: any) {
-    const indexController = new (require('../controllers/index').IndexController)();
-
-    app.get('/', indexController.index);
-    // Add more routes as needed
-}
